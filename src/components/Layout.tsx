b6f9cf77d6fa303678af7caf74e0e72ae21f0f1e
tnfssc <tnfssc@gmail.com>
2021-12-19 11:28:12 +0000

    SEO

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,13 +8,18 @@ export default function Layout({ children }: { children: React.ReactNode }): Rea
   return (
     <>
       <Head>
-        <title>Website</title>
-        <meta name="description" content="" />
+        <title>Random site</title>
+        <meta
+          name="description"
+          content="Just a random website. Probably nothing here is useful. Feel free to explore."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavDrawer />
       <main>
-        <Container maxW={"full"} minH={MainHeight}>{children}</Container>
+        <Container maxW={"full"} minH={MainHeight}>
+          {children}
+        </Container>
       </main>
       <Footer />
     </>

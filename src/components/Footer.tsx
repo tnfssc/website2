2fd7ccc16a8b9224bc3c1db447459d2668c6393f
tnfssc <tnfssc@gmail.com>
2021-12-20 04:29:06 +0000

    Fixed footer overflow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { Icon, IconProps } from "@chakra-ui/icons";
-import { Box, Center, HStack } from "@chakra-ui/react";
-import { FaLinkedin, FaGithub, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
+import { Box, Center, HStack, Wrap, WrapItem } from "@chakra-ui/react";
+import { FaLinkedin, FaGithub, FaTwitter, FaYoutube } from "react-icons/fa";
 import { MdMail } from "react-icons/md";
 import Link from "./Link";
 
@@ -18,29 +18,75 @@ const SocialIcon: React.FC<SocialIconsProps> = ({ href = "#", icon, size = "10" 
 
 export const FooterHeight = "133.5px";
 
+const navItems: { label: string; href: string }[] = [
+  {
+    label: "Projects",
+    href: "/projects",
+  },
+  {
+    label: "Experience",
+    href: "/experience",
+  },
+  {
+    label: "Skills",
+    href: "/skills",
+  },
+  {
+    label: "Academics",
+    href: "/acads",
+  },
+  {
+    label: "Hobbies",
+    href: "/hobbies",
+  },
+];
+
+const socialIcons: { href: string; icon: React.FC }[] = [
+  {
+    href: "https://www.linkedin.com/in/tnfssc",
+    icon: FaLinkedin,
+  },
+  {
+    href: "https://twitter.com/tnfssc",
+    icon: FaTwitter,
+  },
+  {
+    href: "mailto:tnfssc@gmail.com",
+    icon: MdMail,
+  },
+  {
+    href: "https://www.youtube.com/channel/UCuB3HpVwVpNyaVxjr8k5Geg",
+    icon: FaYoutube,
+  },
+  {
+    href: "https://github.com/tnfssc",
+    icon: FaGithub,
+  },
+];
+
 export default function Footer() {
   return (
     <footer>
       <Box borderTop="1px" pt="5">
         <Center>
-          <HStack spacing="5" justifyContent="center">
-            <Link href="/projects">Projects</Link>
-            <Link href="/experience">Experience</Link>
-            <Link href="/skills">Skills</Link>
-            <Link href="/acads">Academics</Link>
-            <Link href="/hobbies">Hobbies</Link>
-          </HStack>
+          <Wrap spacing="5" justify="center">
+            {navItems.map(item => (
+              <WrapItem key={item.label}>
+                <Link href={item.href}>{item.label}</Link>
+              </WrapItem>
+            ))}
+          </Wrap>
         </Center>
       </Box>
       <Box py="5">
         <Center>
-          <HStack spacing="5">
-            <SocialIcon href="https://www.linkedin.com/in/tnfssc" icon={FaLinkedin} />
-            <SocialIcon href="https://twitter.com/tnfssc" icon={FaTwitter} />
-            <SocialIcon href="mailto:tnfssc@gmail.com" icon={MdMail} />
-            <SocialIcon href="https://www.youtube.com/channel/UCuB3HpVwVpNyaVxjr8k5Geg" icon={FaYoutube} />
-            <SocialIcon href="https://github.com/tnfssc" icon={FaGithub} />
-          </HStack>
+          <Wrap spacing={5} justify={"center"}>
+            {socialIcons.map(item => (
+              <WrapItem key={item.href}>
+                <SocialIcon {...item} />
+              </WrapItem>
+            ))}
+          </Wrap>
         </Center>
       </Box>
     </footer>

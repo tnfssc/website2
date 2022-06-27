79cbcea058c7d09391770a4beea3b89fbc3fb37c
Sharath Chandra <tnfssc@gmail.com>
2022-06-28 00:18:17 +0530

    Added 3d

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -3,44 +3,7 @@ import { NextPageWithLayout } from "_/global";
 import Link from "../../src/components/Link";
 import { motion } from "framer-motion";
 
-import {
-  SiCplusplus,
-  SiPython,
-  SiReact,
-  SiGit,
-  SiAndroid,
-  SiNodedotjs,
-  SiCloudflare,
-  SiMysql,
-  SiWindowsxp,
-  SiPhp,
-  SiMongodb,
-  SiTypescript,
-  SiPostgresql,
-  SiFlutter,
-  SiGooglecloud,
-  SiAmazonaws,
-  SiMicrosoftazure,
-  SiMacos,
-  SiNextdotjs,
-  SiFigma,
-  SiFirebase,
-  SiSupabase,
-  SiJupyter,
-  SiAtlassian,
-  SiGithub,
-  SiVercel,
-  SiSvelte,
-  SiMaterialui,
-  SiChakraui,
-  SiDart,
-  SiGo,
-  SiArduino,
-  SiRaspberrypi,
-  SiDocker,
-  SiArchlinux,
-  SiUbuntu,
-} from "react-icons/si";
+import { skills } from "../../src/data/skills";
 
 type SkillIconProps = { icon: React.FC; name: string; url: string };
 
@@ -60,45 +23,6 @@ const SkillIcon: React.FC<SkillIconProps> = ({ icon, name, url }) => (
   </motion.span>
 );
 
-const skills: SkillIconProps[] = [
-  { icon: SiTypescript, name: "TypeScript", url: "https://www.typescript.com/" },
-  { icon: SiPhp, name: "PHP", url: "https://www.php.net/" },
-  { icon: SiCplusplus, name: "C++", url: "https://www.cplusplus.com/" },
-  { icon: SiPython, name: "Python", url: "https://www.python.org/" },
-  { icon: SiMongodb, name: "MongoDB", url: "https://www.mongodb.com/" },
-  { icon: SiMysql, name: "MySQL", url: "https://www.mysql.com/" },
-  { icon: SiPostgresql, name: "PostgreSQL", url: "https://www.postgresql.com/" },
-  { icon: SiReact, name: "React", url: "https://reactjs.org/" },
-  { icon: SiNextdotjs, name: "Next.js", url: "https://nextjs.org/" },
-  { icon: SiAndroid, name: "React Native", url: "https://reactnative.dev/" },
-  { icon: SiFlutter, name: "Flutter", url: "https://flutter.dev/" },
-  { icon: SiNodedotjs, name: "Node.JS", url: "https://nodejs.org/" },
-  { icon: SiGit, name: "Git", url: "https://git-scm.com/" },
-  { icon: SiMicrosoftazure, name: "Microsoft Azure", url: "https://azure.microsoft.com/" },
-  { icon: SiGooglecloud, name: "Google Cloud", url: "https://cloud.google.com/" },
-  { icon: SiAmazonaws, name: "Amazon AWS", url: "https://aws.amazon.com/" },
-  { icon: SiCloudflare, name: "Cloudflare", url: "https://cloudflare.com/" },
-  { icon: SiMacos, name: "MacOS", url: "https://www.apple.com/macos/" },
-  { icon: SiWindowsxp, name: "Windows", url: "https://www.windows.com/" },
-  { icon: SiSupabase, name: "Supabase", url: "https://supabase.com/" },
-  { icon: SiJupyter, name: "Jupyter", url: "https://jupyter.org/" },
-  { icon: SiAtlassian, name: "Atlassian", url: "https://www.atlassian.com/" },
-  { icon: SiFigma, name: "Figma", url: "https://www.figma.com/" },
-  { icon: SiFirebase, name: "Firebase", url: "https://firebase.google.com/" },
-  { icon: SiGithub, name: "GitHub", url: "https://github.com/" },
-  { icon: SiVercel, name: "Vercel", url: "https://vercel.com/" },
-  { icon: SiSvelte, name: "Svelte", url: "https://svelte.dev/" },
-  { icon: SiMaterialui, name: "Material UI", url: "https://material-ui.com/" },
-  { icon: SiChakraui, name: "Chakra UI", url: "https://chakra-ui.com/" },
-  { icon: SiDart, name: "Dart", url: "https://dart.dev/" },
-  { icon: SiGo, name: "Go", url: "https://golang.org/" },
-  { icon: SiArduino, name: "Arduino", url: "https://www.arduino.cc/" },
-  { icon: SiRaspberrypi, name: "Raspberry Pi", url: "https://www.raspberrypi.org/" },
-  { icon: SiDocker, name: "Docker", url: "https://www.docker.com/" },
-  { icon: SiArchlinux, name: "Arch Linux", url: "https://www.archlinux.org/" },
-  { icon: SiUbuntu, name: "Ubuntu", url: "https://www.ubuntu.com/" },
-];
-
 const Skills: NextPageWithLayout = () => {
   return (
     <Center>
@@ -107,7 +31,7 @@ const Skills: NextPageWithLayout = () => {
           <Link href="/skills">
             <Heading>Skills</Heading>
           </Link>
-          <Link href="/skills/beta">(checkout the beta)</Link>
+          checkout the <Link href="/skills/beta">beta</Link>, <Link href="/skills/beta">3d</Link>
         </HStack>
         <VStack py="10">
           <Wrap spacing={6} justify="center">
---
+++ src/hooks/useIconCloud.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { renderSimpleIcon, fetchSimpleIcons, SimpleIcon } from "react-icon-cloud";
+
+export const useIconCloud = (slugs: Array<{ slug: string; url: string }>) => {
+  const [icons, setIcons] = useState<{ simpleIcons: Record<string, SimpleIcon> }>();
+  useEffect(() => {
+    fetchSimpleIcons({ slugs: slugs.map(s => s.slug) }).then(setIcons);
+  }, [slugs]);
+
+  if (icons) {
+    return Object.values(icons.simpleIcons).map(icon =>
+      renderSimpleIcon({
+        icon,
+        size: 150,
+        aProps: {
+          target: "_blank",
+          rel: "noopener noreferrer",
+          href: slugs.find(s => s.slug === icon.slug)?.url,
+        },
+      })
+    );
+  }
+
+  return null;
+};
